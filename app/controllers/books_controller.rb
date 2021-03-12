2b530a4f975ca63e1e9f410ad61f2b609f8adfb5
Tatsuya-Inoue926 <t.i.ykts.926@ezweb.ne.jp>
2021-03-12 21:47:40 +0000

    [Finish] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,10 @@ class BooksController < ApplicationController
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to controller: :books, action: :show, id: @book.id
+    if not @book.save
+      flash[:notice] = "Book was error created."
+      redirect_to controller: :books, action: :index
+    end
     else
       @books = Book.all
       render controller: :books, action: :index
@@ -31,6 +35,10 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully updated."
       redirect_to controller: :books, action: :show, id: @book.id
     end
+    if not @book.update(book_params)
+      flash[:notice] = "Book was error updated."
+      redirect_to books_path
+    end
   end
 
   def destroy
@@ -38,6 +46,10 @@ class BooksController < ApplicationController
     if @book.destroy
       flash[:notice] = "Book was successfully destroyed."# データ（レコード）を削除
       redirect_to books_path# 投稿一覧画面へリダイレクト
+    end
+    if not @book.destroy
+      flash[:notice] = "Book was error destroyed."
+      redirect_to controller: :books, action: :index
     end
   end
 
