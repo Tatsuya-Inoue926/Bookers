6f916a85b2b30c29638478f65ef7706a28907f34
Tatsuya-Inoue926 <t.i.ykts.926@ezweb.ne.jp>
2021-03-12 16:22:54 +0000

    [Start] 3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to controller: :books, action: :show, id: @book.id
+    else
+      @books = Book.all
+      render controller: :books, action: :index
+    end
+  end
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to controller: :books, action: :show, id: @book.id
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed."# データ（レコード）を削除
+      redirect_to books_path# 投稿一覧画面へリダイレクト
+    end
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
